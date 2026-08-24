6fac0ab48dd9bd200eb4579e2830bd18e2ef4fc0
Joel Engelman <joeldavidengelman@hotmail.com>
2026-08-25 08:26:45 +1000

    Expose GitHub and Cloudflare APIs safely

--- preload.js
+++ preload.js
@@ -1,9 +1,6 @@
 const { contextBridge, ipcRenderer } = require('electron');
-contextBridge.exposeInMainWorld('flux', {
-  get: key => ipcRenderer.invoke('store:get').then(s => s[key]),
-  set: (key, value) => ipcRenderer.invoke('store:set', key, value),
-  secretSet: (key, value) => ipcRenderer.invoke('secret:set', key, value),
-  secretGet: key => ipcRenderer.invoke('secret:get', key),
-  openExternal: url => ipcRenderer.invoke('open-external', url),
-  info: () => ipcRenderer.invoke('app-info')
+contextBridge.exposeInMainWorld('flux',{
+ get:key=>ipcRenderer.invoke('store:get').then(s=>s[key]),set:(key,value)=>ipcRenderer.invoke('store:set',key,value),secretSet:(key,value)=>ipcRenderer.invoke('secret:set',key,value),secretGet:key=>ipcRenderer.invoke('secret:get',key),openExternal:url=>ipcRenderer.invoke('open-external',url),info:()=>ipcRenderer.invoke('app-info'),
+ github:{me:()=>ipcRenderer.invoke('github:me'),repos:()=>ipcRenderer.invoke('github:listRepos'),tree:(owner,repo,ref)=>ipcRenderer.invoke('github:repoTree',owner,repo,ref),file:(owner,repo,path,ref)=>ipcRenderer.invoke('github:file',owner,repo,path,ref),commit:(owner,repo,path,content,message,sha,branch)=>ipcRenderer.invoke('github:commit',owner,repo,path,content,message,sha,branch)},
+ cloudflare:{accounts:()=>ipcRenderer.invoke('cloudflare:accounts'),workers:()=>ipcRenderer.invoke('cloudflare:workers')}
 });
